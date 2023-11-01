23af4feb999effafb38b512bcb2191754ba5dc7c
d7ry <7ianyun@berkeley.edu>
2023-11-01 01:31:12 -0700

    update playground

--- lua/playground.py
+++ lua/playground.py
@@ -2,6 +2,10 @@ def main():
     print("hello world!")
 
 
+def subroutine(input: float):
+    print(f"subroutine has received value {input}")
+
+
 # github copilot isn't working here...
 if __name__ == "__main__":
     main()  # call main here.
