61e3788b32dccbba96c8609b5185de03a937d72b
d7ry <7ianyun@berkeley.edu>
2023-11-02 01:03:41 -0700

    stuff

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -44,6 +44,12 @@ vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', opts)
 -- Insertion   --
 -----------------
 
+-- Remove the original keybinding for Ctrl-j
+-- vim.api.nvim_del_keymap('i', '<C-j>')
+vim.api.nvim_set_keymap('i', '<C-h>', '<Left>', { noremap = true, silent = true, expr = false })
+--vim.api.nvim_set_keymap('i', '<C-j>', '<Down>', { noremap = true, silent = true, expr = false })
+--vim.api.nvim_set_keymap('i', '<C-k>', '<Up>', { noremap = true, silent = true, expr = false })
+vim.api.nvim_set_keymap('i', '<C-l>', '<Right>', { noremap = true, silent = true, expr = false })
 
 -- Define a keybinding in Insert mode to delete a word with Ctrl+Backspace
 vim.api.nvim_set_keymap('i', '<C-BS>', '<C-w>', { noremap = true, silent = true })
