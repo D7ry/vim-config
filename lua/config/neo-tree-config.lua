aeb1ca0724d0ececf308e9e29ed3dd1757901ed5
d7ry <7ianyun@berkeley.edu>
2023-11-03 15:51:20 -0700

    some keybind changes

--- lua/config/neo-tree-config.lua
+++ lua/config/neo-tree-config.lua
@@ -98,7 +98,7 @@ return {
         nowait = true,
       },
       mappings = {
-        ["<space>"] = { 
+        ["<Tab>"] = { 
             "toggle_node", 
             nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use 
         },
@@ -112,15 +112,15 @@ return {
         ["s"] = "open_vsplit",
         -- ["S"] = "split_with_window_picker",
         -- ["s"] = "vsplit_with_window_picker",
-        ["t"] = "open_tabnew",
+        --["t"] = "open_tabnew",
         -- ["<cr>"] = "open_drop",
         -- ["t"] = "open_tab_drop",
         ["w"] = "open_with_window_picker",
         --["P"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
         ["C"] = "close_node",
         -- ['C'] = 'close_all_subnodes',
-        ["z"] = "close_all_nodes",
-        --["Z"] = "expand_all_nodes",
+        ["e"] = "close_all_nodes",
+        ["E"] = "expand_all_nodes",
         ["a"] = { 
           "add",
           -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
@@ -129,7 +129,7 @@ return {
             show_path = "none" -- "none", "relative", "absolute"
           }
         },
-        ["A"] = "add_directory", -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
+        --["A"] = "add_directory", -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
         ["d"] = "delete",
         ["r"] = "rename",
         ["y"] = "copy_to_clipboard",
