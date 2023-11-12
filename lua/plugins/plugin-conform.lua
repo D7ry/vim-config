08aea79d6e9e3f5ea88d9d4df6fb2a47d4eb08a7
d7ryLinux <yty108102@gmail.com>
2023-11-12 04:09:44 -0800

    add cpp format configuration

--- lua/plugins/plugin-conform.lua
+++ lua/plugins/plugin-conform.lua
@@ -1,39 +1,48 @@
 return {
-    "stevearc/conform.nvim",
-    config = function()
-        require("conform").setup({
-            formatters_by_ft = {
-                lua = { "stylua" },
-                -- Conform will run multiple formatters sequentially
-                python = { "autoflake"},
-                -- Use a sub-list to run only the first available formatter
-                javascript = { { "prettierd", "prettier" } },
-                rust = { "rustfmt" },
-            },
-        })
-    end,
-    lazy = false,
-    keys = {
-        {
-            "<Leader>fm",
-            function()
-                -- Define any options required for formatting
-                local opts = {}
+	"stevearc/conform.nvim",
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				-- Conform will run multiple formatters sequentially
+				python = { "autoflake" },
+				-- Use a sub-list to run only the first available formatter
+				javascript = { { "prettierd", "prettier" } },
+				rust = { "rustfmt" },
+				cpp = { "clang_format" },
+			},
+			formatters = {
+				clang_format = {
+					args = {
+						"-style={BasedOnStyle: LLVM, IndentWidth: 4, ColumnLimit: 100, PointerAlignment: Left}"
+					}
+					-- indentation is a tab
+				},
+			},
+		})
+	end,
+	lazy = false,
+	keys = {
+		{
+			"<Leader>fm",
+			function()
+				-- Define any options required for formatting
+				local opts = {}
 
-                -- Define the callback function
-                local function formatCallback(err)
-                    if err then
-                        -- If err is not nil, an error occurred during formatting.
-                        vim.notify("Formatting failed: " .. err, vim.log.levels.ERROR, {title = "Conform"})
-                    else
-                        -- If err is nil, formatting was successful.
-                        vim.notify("Formatting successful", vim.log.levels.INFO, {title = "Conform"})
-                    end
-                end
+				-- Define the callback function
+				local function formatCallback(err)
+					if err then
+						-- If err is not nil, an error occurred during formatting.
+						vim.notify("Formatting failed: " .. err, vim.log.levels.ERROR, { title = "Conform" })
+					else
+						-- If err is nil, formatting was successful.
+						vim.notify("Formatting successful", vim.log.levels.INFO, { title = "Conform" })
+					end
+				end
 
-                -- Call the format function with options and the callback
-                require("conform").format(opts, formatCallback)
-            end,
-        },
-    },
+				-- Call the format function with options and the callback
+				require("conform").format(opts, formatCallback)
+			end,
+		},
+	},
 }
