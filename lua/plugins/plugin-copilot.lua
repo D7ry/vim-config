0d7ea54a24cf7a18acfdc202bb22004cd96e9554
d7ryLinux <yty108102@gmail.com>
2024-03-22 12:38:26 -0700

    update diffview

--- lua/plugins/plugin-copilot.lua
+++ lua/plugins/plugin-copilot.lua
@@ -23,7 +23,7 @@ return {
 				auto_trigger = true,
 				debounce = 75,
 				keymap = {
-					--accept = "<tab>",
+					accept = "<C-enter>",
 					accept_word = false,
 					accept_line = false,
 					next = "<C-;>",
--- lua/plugins/plugin-diffview.lua
+++ lua/plugins/plugin-diffview.lua
@@ -1,15 +1,22 @@
 local viewing_diff = false
 local viewing_file_history = false
+local viewing_commit_history = false
 return {
 	"sindrets/diffview.nvim",
 	keys = {
+
 		{
 			"<Leader>gd",
 			function()
-				if viewing_diff then
+				if viewing_file_history then
 					vim.cmd("tabclose")
-					viewing_diff = false
-				else
+					viewing_file_history = false
+				end
+				if viewing_commit_history then
+					vim.cmd("tabclose")
+					viewing_commit_history = false
+				end
+				if not viewing_diff then
 					vim.cmd("DiffviewOpen")
 					viewing_diff = true
 				end
@@ -19,10 +26,15 @@ return {
 		{
 			"<Leader>gf",
 			function()
-				if viewing_file_history then
+				if viewing_diff then
 					vim.cmd("tabclose")
-					viewing_file_history = false
-				else
+					viewing_diff = false
+				end
+				if viewing_commit_history then
+					vim.cmd("tabclose")
+					viewing_commit_history = false
+				end
+				if not viewing_file_history then
 					vim.cmd("DiffviewFileHistory %")
 					viewing_file_history = true
 				end
@@ -30,18 +42,45 @@ return {
 			"Toggle file history view for current file",
 		},
 		{
-			"<Leader>gF",
+			"<Leader>gc",
+
 			function()
+				if viewing_diff then
+					vim.cmd("tabclose")
+					viewing_diff = false
+				end
 				if viewing_file_history then
 					vim.cmd("tabclose")
 					viewing_file_history = false
-				else
+				end
+				if not viewing_commit_history then
 					vim.cmd("DiffviewFileHistory")
-					viewing_file_history = true
+					viewing_commit_history = true
 				end
 			end,
-			"Toggle file history view for all files",
+			"Toggle commit history view for all files",
 		},
-        {}
+		{
+			"q",
+			function()
+				-- local fallback = not viewing_diff and not viewing_file_history
+				-- if fallback then
+				--     vim.fn.feedkeys(vim.api.nvim_replace_termcodes("q", true, true, true), '')
+				-- end
+				if viewing_diff then
+					vim.cmd("tabclose")
+					viewing_diff = false
+				end
+				if viewing_file_history then
+					vim.cmd("tabclose")
+					viewing_file_history = false
+				end
+				if viewing_commit_history then
+					vim.cmd("tabclose")
+					viewing_commit_history = false
+				end
+			end,
+			"quit diff view",
+		},
 	},
 }
