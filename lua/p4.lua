f39d6a7745ac9c12718afb31c685b1c529406b54
tian <tianyuny@nvidia.com>
2026-01-25 22:55:08 +0000

    update

--- lua/p4.lua
+++ lua/p4.lua
@@ -12,62 +12,26 @@ vim.api.nvim_create_user_command('P4a', function()
         return
     end
 
-    -- Convert the WSL file path to a Windows file path
-    local cmd_convert_path = 'wslpath -w "' .. current_file .. '"'
-    local windows_file_path = vim.fn.systemlist(cmd_convert_path)[1]
-    -- deobfuscate windows file path
-    windows_file_path = windows_file_path:gsub("\\", "\\\\")
-    --print("Windows file path: " .. windows_file_path)  -- Debug print
+    local p4_exe = "p4"
 
-    -- Define the Perforce annotate command
-    local cmd = 'p4.exe annotate -u -c -q ' .. windows_file_path
-
-    vim.notify(cmd)
-    --print("Command: " .. cmd)  -- Debug print
-
-    -- Run the command and capture the output
-    local output = vim.fn.systemlist(cmd)
-
-    -- Create a new vertical buffer and split window
-    vim.cmd('vnew')
-
-    -- Get the buffer of the new window
-    local buf = vim.api.nvim_get_current_buf()
-
-    -- Set the buffer content to the command output
-    vim.api.nvim_buf_set_lines(buf, 0, -1, false, output)
-
-    -- Set the buffer to be temporary and non-modifiable
-    vim.bo[buf].buftype = 'nofile'
-    vim.bo[buf].bufhidden = 'wipe'
-    vim.bo[buf].modifiable = false
-    vim.bo[buf].readonly = true
-    vim.bo[buf].swapfile = false
-end, {})
-
-
-vim.api.nvim_create_user_command('P4aa', function()
-    -- Get the current buffer number
-    local buf = vim.api.nvim_get_current_buf()
-
-    -- Get the current file path
-    local current_file = vim.api.nvim_buf_get_name(buf)
-    -- print("Current file path: " .. (current_file ~= "" and current_file or "No file associated"))  -- Debug print
-
-    if current_file == "" then
-        vim.notify("No file associated with the current buffer", vim.log.levels.ERROR)
-        return
+	local os_info = vim.loop.os_uname()
+    local file_path = current_file
+	if os_info.sysname == "Windows_NT" then
+        p4_exe = "p4.exe"
+        -- Convert the WSL file path to a Windows file path
+        local cmd_convert_path = 'wslpath -w "' .. current_file .. '"'
+        local windows_file_path = vim.fn.systemlist(cmd_convert_path)[1]
+        -- deobfuscate windows file path
+        windows_file_path = windows_file_path:gsub("\\", "\\\\")
+        --print("Windows file path: " .. windows_file_path)  -- Debug print
+        file_path = windows_file_path
     end
 
-    -- Convert the WSL file path to a Windows file path
-    local cmd_convert_path = 'wslpath -w "' .. current_file .. '"'
-    local windows_file_path = vim.fn.systemlist(cmd_convert_path)[1]
-    -- deobfuscate windows file path
-    windows_file_path = windows_file_path:gsub("\\", "\\\\")
-    --print("Windows file path: " .. windows_file_path)  -- Debug print
-
-    -- Define the Perforce annotate command
-    local cmd = 'p4.exe annotate -a -u -c -q ' .. windows_file_path
+    -- p4 annotate command
+    -- -u -- show user
+    -- -c -- show cl number
+    -- -I -- show integration history(useful for multiple branches)
+    local cmd = p4_exe .. ' annotate -u -I -c ' .. file_path
 
     vim.notify(cmd)
     --print("Command: " .. cmd)  -- Debug print
